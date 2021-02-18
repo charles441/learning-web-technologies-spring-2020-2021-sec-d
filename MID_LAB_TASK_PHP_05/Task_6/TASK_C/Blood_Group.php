24bce36573720bd2addcd24766ac382c7f67482d
charles441 <malikalmulk97@gmail.com>
2021-02-18 23:58:14 +0600

    php

---
+++ MID_LAB_TASK_PHP_05/Task_6/TASK_C/Blood_Group.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+	<title>blood group Input field</title>
+</head>
+<body>
+	<form method="POST" action="#">
+		<fieldset>
+			
+		<legend>Blood Group </legend>
+<select id="group" name="group">
+	<option value="def">  Select Group  </option>
+	<option value="1">A+</option>
+	<option value="2">A-</option>
+	<option value="3">B-</option>
+	<option value="4">B+</option>
+	<option value="5">O+</option>
+	<option value="6">O-</option>
+	<option value="7">AB+</option>
+</select>
+<br> <br>
+			<input type="submit" name="submit" value="Submit">
+		</fieldset>
+	</form>
+</body>
+</html>
