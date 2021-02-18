878a99eca054a2bb79c8d8227385b1ee7fc41c41
charles441 <malikalmulk97@gmail.com>
2021-02-18 23:55:37 +0600

    php

---
+++ MID_LAB_TASK_PHP_05/Task_5/TASK_C/Degree.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+	<title>Degree Input field</title>
+</head>
+<body>
+	<form method="POST" action="#">
+		<fieldset>
+			
+			<legend>Degree</legend>
+			<input type="checkbox"  name="degree[]" value="SSC">SSC
+			<input type="checkbox" name="degree[]" value="HSC">HSC
+			<input type="checkbox" name="degree[]" value="BSC">BSC
+			<input type="checkbox" name="degree[]" value="MSC"> MSC <br> <br>
+						<input type="submit" name="submit" value="Submit">
+		</fieldset>
+	</form>
+</body>
+</html>
